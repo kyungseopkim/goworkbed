0b1bd2afd69c500a731826e13c49786386cf85af
Jerry Kim <jerrykim@lucidmotors.com>
2020-04-04 05:26:59 -0700

    change kafkaFilter

--- kafka/kafkaFilter.go
+++ kafka/kafkaFilter.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 	"log"
 	"strings"
+
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 )
 
-func main()  {
+func main() {
 	options := GetFilterOptions()
 	log.Println(options)
 
@@ -17,20 +18,17 @@ func main()  {
 		"security.protocol": options.Protocol,
 	})
 	defer c.Close()
+	if err != nil {
+		panic(err)
+	}
 
-	p, er := kafka.NewProducer(&kafka.ConfigMap{
+	producer, er := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": options.Brokers,
 		"security.protocol": options.Protocol,
-		})
+	})
 
 	if er != nil {
 		log.Fatal(er)
-	}
-
-	defer p.Close()
-
-	if err != nil {
-		panic(err)
 	}
 
 	filter := make(map[string]bool)
@@ -38,29 +36,43 @@ func main()  {
 		filter[v] = true
 	}
 
-	c.SubscribeTopics([]string{ options.Topic }, nil)
+	c.SubscribeTopics([]string{options.Topic}, nil)
 
 	topic := options.TargetTopic
-	counter := 0
+
+	// Delivery report handler for produced messages
+	go func() {
+		count := 0
+		for e := range producer.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					log.Fatalf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					if count == 100 {
+						log.Printf("message %s\n", ev.Value)
+						count = 0
+					}
+					count++
+				}
+			}
+		}
+	}()
+
 	for {
 		msg, err := c.ReadMessage(-1)
 		if err == nil {
-			 signal := SignalFromJson(msg.Value)
-			 if _ , ok := filter[signal.SignalName]; ok {
-					p.Produce(&kafka.Message{
-						TopicPartition: kafka.TopicPartition{Topic:&topic , Partition: kafka.PartitionAny},
-						Value:          signal.JSON(),
-					},nil)
-					if counter == 10000 {
-						log.Println(signal)
-						counter = 0
-					}
-					counter++
-			 }
+			signal := SignalFromJson(msg.Value)
+			if _, ok := filter[signal.SignalName]; ok {
+				producer.Produce(&kafka.Message{
+					TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+					Value:          signal.JSON(),
+					Headers:        []kafka.Header{{Key: "kafkaSelectedSignals", Value: []byte("header values are binary")}},
+				}, nil)
+			}
 		} else {
 			// The client will automatically try to recover from all errors.
 			log.Printf("Consumer error: %v (%v)\n", err, msg)
 		}
 	}
-
 }
