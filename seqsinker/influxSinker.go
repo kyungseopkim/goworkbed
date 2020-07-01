0b4f17a652575630472d9871229ae9dd5150afa7
Jerry Kim <jerrykim@lucidmotors.com>
2020-07-01 09:34:05 -0700

    use s3 v2 vehicles

--- seqsinker/influxSinker.go
+++ seqsinker/influxSinker.go
@@ -1,135 +1,134 @@
 package main
 
 import (
-    "fmt"
-    _ "github.com/influxdata/influxdb1-client" // this is important because of the bug in go mod
-    client "github.com/influxdata/influxdb1-client/v2"
-    "gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
-    "log"
-    "strconv"
-    "time"
+	"fmt"
+	_ "github.com/influxdata/influxdb1-client" // this is important because of the bug in go mod
+	client "github.com/influxdata/influxdb1-client/v2"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"log"
+	"strconv"
+	"time"
 )
 
 func InfluxSinker(influx client.Client, data []*KafkaMessage, options SinkerOptions) {
-    conf := client.BatchPointsConfig{Precision: "ms",
-        Database:         options.InfluxDB,
-        RetentionPolicy:  options.Retension,
-        WriteConsistency: "any",
-    }
+	conf := client.BatchPointsConfig{Precision: "ms",
+		Database:         options.InfluxDB,
+		RetentionPolicy:  options.Retension,
+		WriteConsistency: "any",
+	}
 
-    v2 := make(V2Vehicle)
-    v2.FromEnv()
+	v2 := make(V2Vehicle)
+	v2.FromS3()
 
-    bp, err := client.NewBatchPoints(conf)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	bp, err := client.NewBatchPoints(conf)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    points := make([]*client.Point, 0)
-    for _, msg := range data {
-        payload := PayloadFromKafkaMessage(msg, v2)
-        if payload == nil {
-            continue
-        }
-        tags := make(map[string]string)
+	points := make([]*client.Point, 0)
+	for _, msg := range data {
+		payload := PayloadFromKafkaMessage(msg, v2)
+		if payload == nil {
+			continue
+		}
+		tags := make(map[string]string)
 
-        tags["vin"] = payload.Vin
-        tags["vlan"] = strconv.Itoa(int(payload.Vlan))
-        landingSec:= payload.LandingTs / 1000
-        landing := time.Unix(landingSec, 0)
-        tags["landingTs"] = fmt.Sprintf("%4d-%02d-%02d", landing.Year(), landing.Month(), landing.Day())
-        fields := make(map[string]interface{})
-        fields["value"] = payload.Seq
-        sec := payload.Ts / 1000000
-        ns := (payload.Ts % 1000000) * 1000
-        tz := time.Unix(sec, ns)
+		tags["vin"] = payload.Vin
+		tags["vlan"] = strconv.Itoa(int(payload.Vlan))
+		landingSec := payload.LandingTs / 1000
+		landing := time.Unix(landingSec, 0)
+		tags["landingTs"] = fmt.Sprintf("%4d-%02d-%02d", landing.Year(), landing.Month(), landing.Day())
+		fields := make(map[string]interface{})
+		fields["value"] = payload.Seq
+		sec := payload.Ts / 1000000
+		ns := (payload.Ts % 1000000) * 1000
+		tz := time.Unix(sec, ns)
 
-        point, er := client.NewPoint(options.Measurement, tags, fields, tz)
-        if er != nil {
-            log.Println(er)
-        }
-        points = append(points, point)
-    }
+		point, er := client.NewPoint(options.Measurement, tags, fields, tz)
+		if er != nil {
+			log.Println(er)
+		}
+		points = append(points, point)
+	}
 
-    if len(points) > 0 {
-        bp.AddPoints(points)
-        log.Println(fmt.Sprintf("writing %d items", len(points)))
-        err = influx.Write(bp)
-        if err != nil {
-            log.Println(err)
-        }
-    }
+	if len(points) > 0 {
+		bp.AddPoints(points)
+		log.Println(fmt.Sprintf("writing %d items", len(points)))
+		err = influx.Write(bp)
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
 
 func main() {
-    options := GetSinkerOptions()
-    log.Println(options)
+	options := GetSinkerOptions()
+	log.Println(options)
 
-    c, err := kafka.NewConsumer(&kafka.ConfigMap{
-        "bootstrap.servers": options.Brokers,
-        "group.id":          options.GroupID,
-        "auto.offset.reset": options.OffsetReset,
-        "security.protocol": options.Protocol,
-        //"max.poll.records":  options.MaxRecords,
-    })
-    if err != nil {
-        panic(err)
-    }
-    defer c.Close()
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": options.Brokers,
+		"group.id":          options.GroupID,
+		"auto.offset.reset": options.OffsetReset,
+		"security.protocol": options.Protocol,
+		//"max.poll.records":  options.MaxRecords,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
 
+	influxUrl := fmt.Sprintf("http://%s", options.InfluxServer)
+	influx, er := client.NewHTTPClient(client.HTTPConfig{
+		Addr: influxUrl,
+	})
+	if er != nil {
+		log.Println("Error creating InfluxDB Client: ", err.Error())
+	}
+	defer influx.Close()
 
-    influxUrl := fmt.Sprintf("http://%s", options.InfluxServer)
-    influx, er := client.NewHTTPClient(client.HTTPConfig{
-        Addr: influxUrl,
-    })
-    if er != nil {
-        log.Println("Error creating InfluxDB Client: ", err.Error())
-    }
-    defer influx.Close()
+	maxBuffer, er1 := strconv.Atoi(options.Update)
+	if er1 != nil {
+		log.Fatalln(err)
+	}
 
-    maxBuffer, er1 := strconv.Atoi(options.Update)
-    if er1 != nil {
-        log.Fatalln(err)
-    }
+	err = c.SubscribeTopics([]string{options.Topic}, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    err = c.SubscribeTopics([]string{options.Topic}, nil)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	limit, err := strconv.ParseInt(options.Duration, 10, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    limit, err := strconv.ParseInt(options.Duration, 10, 32)
-    if err != nil {
-        log.Fatal(err)
-    }
+	prevCheck := time.Now()
+	prevSync := time.Now()
 
-    prevCheck := time.Now()
-    prevSync := time.Now()
-
-    buffer := make([]*KafkaMessage, 0, maxBuffer)
-    for {
-        msg, err := c.ReadMessage(-1)
-        if err == nil {
-            kafkaMsg := KafkaMessageFromJson(msg.Value)
-            if kafkaMsg == nil {
-                continue
-            }
-            buffer = append(buffer, kafkaMsg)
-            current := time.Now()
-            if len(buffer) == maxBuffer || current.Sub(prevSync).Seconds() > float64(limit) {
-                bufferClone := make([]*KafkaMessage, len(buffer))
-                copy(bufferClone, buffer)
-                go InfluxSinker(influx, bufferClone, options)
-                buffer = buffer[:0]
-                now := time.Now()
-                if now.Sub(prevCheck).Seconds() > 10 {
-                    //log.Println("working:" + signal.String())
-                    prevCheck = now
-                }
-                prevSync = current
-            }
-        } else {
-            // The client will automatically try to recover from all errors.
-            log.Printf("Consumer error: %v (%v)\n", err, msg)
-        }
-    }
+	buffer := make([]*KafkaMessage, 0, maxBuffer)
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			kafkaMsg := KafkaMessageFromJson(msg.Value)
+			if kafkaMsg == nil {
+				continue
+			}
+			buffer = append(buffer, kafkaMsg)
+			current := time.Now()
+			if len(buffer) == maxBuffer || current.Sub(prevSync).Seconds() > float64(limit) {
+				bufferClone := make([]*KafkaMessage, len(buffer))
+				copy(bufferClone, buffer)
+				go InfluxSinker(influx, bufferClone, options)
+				buffer = buffer[:0]
+				now := time.Now()
+				if now.Sub(prevCheck).Seconds() > 10 {
+					//log.Println("working:" + signal.String())
+					prevCheck = now
+				}
+				prevSync = current
+			}
+		} else {
+			// The client will automatically try to recover from all errors.
+			log.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
 }
--- seqsinker/v2vehicle.go
+++ seqsinker/v2vehicle.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-    "os"
-    "strings"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 )
 
 type V2Vehicle map[string]struct{}
@@ -10,25 +14,48 @@ type V2Vehicle map[string]struct{}
 var exists = struct{}{}
 
 func (v2 V2Vehicle) String() string {
-    return v2.String()
+	return v2.String()
 }
 
 func (v2 V2Vehicle) Contains(key string) bool {
-    _, ok := v2[key]
-    if ok {
-        return true
-    }
-    return false
+	_, ok := v2[key]
+	if ok {
+		return true
+	}
+	return false
 }
 
-func (v2 V2Vehicle) FromString(cars string ) {
-    for _, item := range strings.Split(cars, ",") {
-        v2[item]=exists
-    }
-}
+func (v2 V2Vehicle) FromS3() {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
 
-func (v2 V2Vehicle) FromEnv() {
-    values := os.Getenv("V2VEHICLES")
-    v2.FromString(values)
-}
+	bucketName := "java-library-repository"
+	keyName := "resources/v2/v2vehicles.yaml"
+	// Create S3 service client
+	svc := s3.New(sess)
+	req, resp := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(keyName),
+	})
 
+	err = req.Send()
+	if err != nil { // resp is now filled
+		log.Fatalln(err)
+	}
+
+	objBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//v2s := V2Vehicles{}
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(objBytes, &m)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, v := range m["vehicles"].([]string) {
+		v2[v] = exists
+	}
+}
